44d406aef73516fdf4865c6c9efcd47a70f599f4
bhupeshwar <39058138+bhupeshwar@users.noreply.github.com>
2019-02-07 20:00:27 +0530

    a

--- lambda/bibot_config.py
+++ lambda/bibot_config.py
@@ -20,7 +20,7 @@ TOP_RESOLUTION = 1
 
 SLOT_CONFIG = {
     'dl_name':       {'type': TOP_RESOLUTION, 'remember': True,  'error': 'I couldn\'t find an DL called "{}".'},
-    'event_month':      {'type': ORIGINAL_VALUE, 'remember': True},
+    'dl_date':      {'type': ORIGINAL_VALUE, 'remember': True},
     'job_date':      {'type': ORIGINAL_VALUE, 'remember': True},
     'template_name':         {'type': TOP_RESOLUTION, 'remember': True,  'error': 'I couldn\'t find a template called "{}".'},
     'count':            {'type': ORIGINAL_VALUE, 'remember': True},
@@ -33,7 +33,7 @@ SLOT_CONFIG = {
 
 DIMENSIONS = {
     'dl_name':     {'slot': 'dl_name',  'column': 'dl.dl_name',  'singular': 'dl_name'},
-    'months':     {'slot': 'event_month', 'column': 'd.month',       'singular': 'month'},
+    'dl_date':     {'slot': 'dl_date', 'column': 'd.cal_date',       'singular': 'dl_date'},
     'template_name':     {'slot': 'template_name',  'column': 'dmd.template_name',  'singular': 'template_name'}
 }
 
