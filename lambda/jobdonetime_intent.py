01a2094bc04dc7a3f2a5203d49b92dee41853488
bhupeshwar <39058138+bhupeshwar@users.noreply.github.com>
2019-02-08 02:43:42 +0530

    a

--- lambda/jobdonetime_intent.py
+++ lambda/jobdonetime_intent.py
@@ -25,7 +25,7 @@ import bibot_userexits as userexits
 # SELECT statement for JOB_DONE query
 JOB_DONE_SELECT = "SELECT DL.end_date from BA_DL as DL"
 JOB_DONE_JOIN = " WHERE DL.status != 'W'  "
-JOB_DONE_DATE = " AND date_format({}, '%Y-%m-%d')  =  date_format({}, '%Y-%m-%d') "
+JOB_DONE_DATE = " AND date_format({}, '%Y-%m-%d')  =  date_format(timestamp'{}', '%Y-%m-%d') "
 JOB_DONE_WHERE = " AND LOWER({}) LIKE LOWER('%{}%') "
 JOB_DONE_PHRASE = 'job completion date/time'
 
@@ -89,12 +89,13 @@ def jobdone_intent_handler(intent_request, session_attributes):
         if slot_key == 'job_date':
             where_clause += JOB_DONE_DATE.format('DL.end_date',slot_values[slot_key])
         if slot_values[slot_key] is not None:
-            value = userexits.pre_process_query_value(slot_key, slot_values[slot_key])
-            where_clause += JOB_DONE_WHERE.format(bibot.DIMENSIONS.get(dimension).get('column'), value)
+            if slot_key != 'job_date':
+                value = userexits.pre_process_query_value(slot_key, slot_values[slot_key])
+                where_clause += JOB_DONE_WHERE.format(bibot.DIMENSIONS.get(dimension).get('column'), value)
 
     query_string = select_clause + where_clause
-
-     count = query_string
+    """
+    count = query_string
     """
     response = helpers.execute_athena_query(query_string)
 
@@ -103,7 +104,7 @@ def jobdone_intent_handler(intent_request, session_attributes):
         count = result['VarCharValue']
     else:
         count = 0
-    """
+
 
     logger.debug('<<BIBot>> "Count value is: %s' % count)
 
