d309d3d45cbcf602d030e2966cff98f06faced5b
bhupeshwar <39058138+bhupeshwar@users.noreply.github.com>
2019-02-08 15:30:38 +0530

    a

--- lambda/bibot_config.py
+++ lambda/bibot_config.py
@@ -22,16 +22,18 @@ SLOT_CONFIG = {
     'dl_name':       {'type': TOP_RESOLUTION, 'remember': True,  'error': 'I couldn\'t find an DL called "{}".'},
     'month':      {'type': ORIGINAL_VALUE, 'remember': True},
     'dl_date':      {'type': ORIGINAL_VALUE, 'remember': True},
+    'sequence_name':      {'type': ORIGINAL_VALUE, 'remember': True},
     'job_date':      {'type': ORIGINAL_VALUE, 'remember': True},
     'template_name':         {'type': TOP_RESOLUTION, 'remember': True,  'error': 'I couldn\'t find a template called "{}".'},
     'count':            {'type': ORIGINAL_VALUE, 'remember': True},
-    'dimension':        {'type': ORIGINAL_VALUE, 'remember': True}    
+    'dimension':        {'type': ORIGINAL_VALUE, 'remember': True}
 }
 
 DIMENSIONS = {
     'dl_name':     {'slot': 'dl_name',  'column': 'dl.dl_name',  'singular': 'dl_name'},
     'month':     {'slot': 'dl_date', 'column': 'd.month',       'singular': 'month'},
     'dl_date':     {'slot': 'dl_date', 'column': 'dl.end_date',       'singular': 'dl_date'},
+    'sequence_name':     {'slot': 'sequence_name', 'column': 'dmd.sequence_name',       'singular': 'sequence_name'},
     'job_date':     {'slot': 'job_date', 'column': 'dl.end_date',       'singular': 'job_date'},
     'template_name':     {'slot': 'template_name',  'column': 'dmd.template_name',  'singular': 'template_name'}
 }
